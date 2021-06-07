4451e7efb18ab9788c5ceb399926f5e147f5ae28
Eliezer Graber <eygraber@gmail.com>
2021-06-06 22:35:29 -0400

    Try setting version at the top level

--- formatting/build.gradle.kts
+++ formatting/build.gradle.kts
@@ -2,33 +2,29 @@ import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
-    id("org.jetbrains.dokka") version "1.4.32"
-    id("com.vanniktech.maven.publish") version "0.15.1"
+  kotlin("jvm") version "1.5.10"
+  id("org.jetbrains.dokka") version "1.4.32"
+  id("com.vanniktech.maven.publish") version "0.15.1"
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("io.gitlab.arturbosch.detekt:detekt-api:1.17.1")
-    testImplementation("io.gitlab.arturbosch.detekt:detekt-test:1.17.1")
-    testImplementation("junit:junit:4.13")
-    testImplementation("org.assertj:assertj-core:3.19.0")
+  implementation("io.gitlab.arturbosch.detekt:detekt-api:1.17.1")
+  testImplementation("io.gitlab.arturbosch.detekt:detekt-test:1.17.1")
+  testImplementation("junit:junit:4.13")
+  testImplementation("org.assertj:assertj-core:3.19.0")
 }
 
 tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = "11"
-        allWarningsAsErrors = true
-    }
+  kotlinOptions {
+    jvmTarget = "11"
+    allWarningsAsErrors = true
+  }
 }
 
 mavenPublish {
-    sonatypeHost = SonatypeHost.S01
-
-    nexus {
-        stagingProfile = "com.eygraber"
-    }
+  sonatypeHost = SonatypeHost.S01
 }
