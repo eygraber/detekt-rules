a06eeb255b8ae1d6c6d9522760a379f3f1d4b9ad
Eliezer Graber <me@eygraber.com>
2026-01-27 02:01:25 -0500

    Update API for latest Detekt v2 alpha

--- formatting/src/main/kotlin/com/eygraber/detekt/rules/formatting/FormattingRuleSetProvider.kt
+++ formatting/src/main/kotlin/com/eygraber/detekt/rules/formatting/FormattingRuleSetProvider.kt
@@ -2,10 +2,11 @@ package com.eygraber.detekt.rules.formatting
 
 import dev.detekt.api.RuleName
 import dev.detekt.api.RuleSet
+import dev.detekt.api.RuleSetId
 import dev.detekt.api.RuleSetProvider
 
 public class FormattingRuleSetProvider : RuleSetProvider {
-  override val ruleSetId: RuleSet.Id = RuleSet.Id("formatting-eygraber")
+  override val ruleSetId: RuleSetId = RuleSetId("formatting-eygraber")
 
   override fun instance(): RuleSet = RuleSet(
     id = ruleSetId,
