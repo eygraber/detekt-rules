a13a82e73daa4e30b579b10960dacc7271371f1f
Eliezer Graber <me@eygraber.com>
2026-01-27 02:01:25 -0500

    Update API for latest Detekt v2 alpha

--- style/src/main/kotlin/com/eygraber/detekt/rules/style/StyleRuleSetProvider.kt
+++ style/src/main/kotlin/com/eygraber/detekt/rules/style/StyleRuleSetProvider.kt
@@ -2,10 +2,11 @@ package com.eygraber.detekt.rules.style
 
 import dev.detekt.api.RuleName
 import dev.detekt.api.RuleSet
+import dev.detekt.api.RuleSetId
 import dev.detekt.api.RuleSetProvider
 
 public class StyleRuleSetProvider : RuleSetProvider {
-  override val ruleSetId: RuleSet.Id = RuleSet.Id("style-eygraber")
+  override val ruleSetId: RuleSetId = RuleSetId("style-eygraber")
 
   override fun instance(): RuleSet = RuleSet(
     id = ruleSetId,
